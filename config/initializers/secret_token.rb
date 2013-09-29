a8e13950454f512d9ab8337f86eae36e7a08951f
peter942 <petoriaomalley@gmail.com>
2013-09-29 23:23:18 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scaffolding::Application.config.secret_key_base = '4b5e5c045c6aa61317d87ed01a76a18d50299428120b51262e4f04ac32a92a6b1366d8562c2a543a599cc9e9bfa63dbd83b5a179c25571a1105ee9a004e4c5b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scaffolding::Application.config.session_store :cookie_store, key: '_scaffolding_session'
